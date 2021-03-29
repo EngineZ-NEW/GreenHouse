a33e90b195325b8f5fcb37855fdd52c2169df0a3
HRABOVENSKYI <teodor.gr.2002@gmail.com>
2021-03-29 16:27:02 +0300

    Initialized project, added code to application.properties

---
+++ src/main/java/ua/lviv/iot/GreenHouse/GreenHouseApplication.java
@@ -0,0 +1,13 @@
+package ua.lviv.iot.GreenHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/lviv/iot/GreenHouse/GreenHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.iot.GreenHouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
