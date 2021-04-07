a1300e48dad938a3bb6fdb3bbe979f8504fcc7c6
HRABOVENSKYI <teodor.gr.2002@gmail.com>
2021-04-07 11:40:11 +0300

    Added more functionality to Sensor data controller and created requests for 3 sensor types

--- src/main/java/ua/lviv/iot/GreenHouse/controllers/SensorDataController.java
+++ src/main/java/ua/lviv/iot/GreenHouse/controllers/SensorDataController.java
@@ -13,34 +13,110 @@ public class SensorDataController {
 
     private final SensorDataService sensorDataService;
 
+
+    private static final int TEMPERATURE_SENSOR_ID = 102;
+    private static final int HUMIDITY_SENSOR_ID = 103;
+    private static final int LUMINOSITY_SENSOR_ID = 104;
+
+
     @Autowired
     public SensorDataController(SensorDataService sensorDataService) {
         this.sensorDataService = sensorDataService;
     }
 
     @GetMapping
-    public List<SensorData> getSensorData() {
-        return sensorDataService.getSensorData();
+    public List<SensorData> getAllSensorData() {
+        return sensorDataService.getAllSensorData();
+    }
+
+    @GetMapping("/temperature-data")
+    public List<SensorData> getTemperature() {
+        return sensorDataService.getSensorData(TEMPERATURE_SENSOR_ID);
+    }
+
+    @GetMapping("/humidity-data")
+    public List<SensorData> getHumidity() {
+        return sensorDataService.getSensorData(HUMIDITY_SENSOR_ID);
+    }
+
+    @GetMapping("/luminosity-data")
+    public List<SensorData> getLuminosity() {
+        return sensorDataService.getSensorData(LUMINOSITY_SENSOR_ID);
     }
 
     // date should be in format yyyy-mm-dd
     @GetMapping("/{date}")
-    public List<SensorData> getSensorDataForDay(final @PathVariable("date") String date) {
-        return sensorDataService.getSensorDataForDate(date);
+    public List<SensorData> getAllSensorDataForDay(final @PathVariable("date") String date) {
+        return sensorDataService.getAllSensorDataForDate(date);
     }
 
-    @PostMapping
-    public SensorData addSensorData(final @RequestBody SensorData sensorData) {
-        return sensorDataService.createSensorData(sensorData);
+    @GetMapping("/temperature-data/{date}")
+    public List<SensorData> getTemperatureForDay(final @PathVariable("date") String date) {
+        return sensorDataService.getSensorDataForDate(date, TEMPERATURE_SENSOR_ID);
+    }
+
+    @GetMapping("/humidity-data/{date}")
+    public List<SensorData> getHumidityDay(final @PathVariable("date") String date) {
+        return sensorDataService.getSensorDataForDate(date, HUMIDITY_SENSOR_ID);
+    }
+
+    @GetMapping("/luminosity-data/{date}")
+    public List<SensorData> getLuminosityForDay(final @PathVariable("date") String date) {
+        return sensorDataService.getSensorDataForDate(date, LUMINOSITY_SENSOR_ID);
+    }
+
+    @PostMapping("/temperature-data")
+    public SensorData addTemperatureData(final @RequestBody SensorData sensorData) {
+        return sensorDataService.createSensorData(sensorData, TEMPERATURE_SENSOR_ID);
+    }
+
+    @PostMapping("/humidity-data")
+    public SensorData addHumidityData(final @RequestBody SensorData sensorData) {
+        return sensorDataService.createSensorData(sensorData, HUMIDITY_SENSOR_ID);
+    }
+
+    @PostMapping("/luminosity-data")
+    public SensorData addLuminosityData(final @RequestBody SensorData sensorData) {
+        return sensorDataService.createSensorData(sensorData, LUMINOSITY_SENSOR_ID);
     }
 
     @DeleteMapping
-    public void deleteSensorData() {
-        sensorDataService.deleteSensorData();
+    public void deleteAllSensorData() {
+        sensorDataService.deleteAllSensorData();
+    }
+
+    @DeleteMapping("/temperature-data")
+    public void deleteTemperature() {
+        sensorDataService.deleteSensorData(TEMPERATURE_SENSOR_ID);
+    }
+
+    @DeleteMapping("/humidity-data")
+    public void deleteHumidity() {
+        sensorDataService.deleteSensorData(HUMIDITY_SENSOR_ID);
+    }
+
+    @DeleteMapping("/luminosity-data")
+    public void deleteLuminosity() {
+        sensorDataService.deleteSensorData(LUMINOSITY_SENSOR_ID);
     }
 
     @DeleteMapping("/{date}")
     public void deleteSensorDataForDay(final @PathVariable("date") String date) {
-        sensorDataService.deleteSensorDataForDate(date);
+        sensorDataService.deleteAllSensorDataForDate(date);
+    }
+
+    @DeleteMapping("/temperature-data/{date}")
+    public void deleteTemperatureForDay(final @PathVariable("date") String date) {
+        sensorDataService.deleteSensorDataForDate(date, TEMPERATURE_SENSOR_ID);
+    }
+
+    @DeleteMapping("/humidity-data/{date}")
+    public void deleteHumidityForDay(final @PathVariable("date") String date) {
+        sensorDataService.deleteSensorDataForDate(date, HUMIDITY_SENSOR_ID);
+    }
+
+    @DeleteMapping("/luminosity-data/{date}")
+    public void deleteLuminosityForDay(final @PathVariable("date") String date) {
+        sensorDataService.deleteSensorDataForDate(date, LUMINOSITY_SENSOR_ID);
     }
 }
--- src/main/java/ua/lviv/iot/GreenHouse/services/SensorDataService.java
+++ src/main/java/ua/lviv/iot/GreenHouse/services/SensorDataService.java
@@ -19,12 +19,13 @@ public class SensorDataService {
         this.sensorDataRepository = sensorDataRepository;
     }
 
-    public List<SensorData> getSensorData() {
+    public List<SensorData> getAllSensorData() {
         return sensorDataRepository.findAll();
     }
 
-    public SensorData createSensorData(SensorData sensorData) {
-        for (SensorData savedSensorData : sensorDataRepository.findAll()) {
+    public SensorData createSensorData(SensorData sensorData, int sensorId) {
+        sensorData.setSensorId(sensorId);
+        for (SensorData savedSensorData : sensorDataRepository.findSensorDataBySensorId(sensorId)) {
             if (savedSensorData.getTimestamp().equals(sensorData.getTimestamp())) {
                 throw new IllegalStateException("There already is Data for this time!");
             }
@@ -33,11 +34,11 @@ public class SensorDataService {
         return sensorData;
     }
 
-    public void deleteSensorData() {
+    public void deleteAllSensorData() {
         sensorDataRepository.deleteAll();
     }
 
-    public List<SensorData> getSensorDataForDate(String date) {
+    public List<SensorData> getAllSensorDataForDate(String date) {
         List<SensorData> responseSensorData = new ArrayList<>();
         Calendar calendar = Calendar.getInstance();
         for (SensorData sensorData : sensorDataRepository.findAll()) {
@@ -51,8 +52,36 @@ public class SensorDataService {
         return responseSensorData;
     }
 
-    public void deleteSensorDataForDate(String date) {
-        for (SensorData sensorData : getSensorDataForDate(date)) {
+    public void deleteAllSensorDataForDate(String date) {
+        for (SensorData sensorData : getAllSensorDataForDate(date)) {
+            sensorDataRepository.delete(sensorData);
+        }
+    }
+
+    public List<SensorData> getSensorData(int sensorId) {
+        return sensorDataRepository.findSensorDataBySensorId(sensorId);
+    }
+
+    public List<SensorData> getSensorDataForDate(String date, int sensorId) {
+        List<SensorData> responseSensorData = new ArrayList<>();
+        Calendar calendar = Calendar.getInstance();
+        for (SensorData sensorData : sensorDataRepository.findSensorDataBySensorId(sensorId)) {
+            long timestamp = sensorData.getTimestamp().getTime();
+            calendar.setTimeInMillis(timestamp);
+            String receivedDate = String.format("%04d-%02d-%02d", calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH));
+            if (receivedDate.equals(date))
+                responseSensorData.add(sensorData);
+        }
+        return responseSensorData;
+    }
+
+    public void deleteSensorData(int sensorId) {
+        sensorDataRepository.deleteSensorDataBySensorId(sensorId);
+    }
+
+    public void deleteSensorDataForDate(String date, int sensorId) {
+        for (SensorData sensorData : getSensorDataForDate(date, sensorId)) {
             sensorDataRepository.delete(sensorData);
         }
     }
